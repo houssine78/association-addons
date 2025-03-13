592548673831e7bbdccf65238b5cc6d6cc87bd0d
Houssine Bakkali <houssine.bakkali@gmail.com>
2025-03-13 17:13:36 +0100

    [IMP] add module to display bank statement line menu

---
+++ custom/custom_account/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 Open Architects Consulting SRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Custom account",
+    "summary": "customisation for accounting",
+    "version": "18.0.1.0.0",
+    "category": "Account",
+    "author": "Open Architects Consulting SRL, Houssine Bakkali",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "account_statement_base",
+        "account_usability"
+    ],
+    "data": [
+        "views/bank_statement_line.xml",
+    ],
+    "installable": True,
+}
